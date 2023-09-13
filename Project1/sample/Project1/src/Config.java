e83394686a1d9c94f6eef290972e54a797b3109f
Yu Feng <yfjobs97@gmail.com>
2023-09-12 23:30:27 -0700

    Delete Project1 directory

--- Project1/sample/Project1/src/Config.java
+++
@@ -1,9 +0,0 @@
-public class Config {
-    public static int BLOCK_SIZE = 1024;
-    public static final int MODE_SAMPLE = 0;
-    public static final int MODE_EFS = 1;
-
-
-    public static int MODE = MODE_SAMPLE;
-    //public static int MODE = MODE_EFS;
-}
--- Project1/sample/Project1/src/PasswordIncorrectException.java
+++
@@ -1,6 +0,0 @@
-public class PasswordIncorrectException extends Exception{
-    public PasswordIncorrectException()
-    {
-        super();
-    }
-}
